2b4a36af8e9bdcfcbd0ec3c3db52d6c81c2c7f53
Dengke Liu <dengkeliu92@gmail.com>
2021-08-05 15:22:29 -0700

    Update PrefixSum.java

--- PrefixSum.java
+++ PrefixSum.java
@@ -1,7 +1,7 @@
-public class PrefixSum {
+public class PrefixSum 
 
-    // prefixSum[i] = A[0] + A[1] + ... + A[i-1] the sum of first i numbers
-	// sum from A[i] to A[j] is prefixSum[j+1] - prefixSum[i]
+// prefixSum[i] = A[0] + A[1] + ... + A[i-1] the sum of first i numbers
+// sum from A[i] to A[j] is prefixSum[j+1] - prefixSum[i]
 
 	int[] A = [1, 2, 3, 4, 5];
 	// prefixSum - [0, 1, 3, 6, 10, 15]
