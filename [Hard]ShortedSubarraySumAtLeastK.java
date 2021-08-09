fa045f0fb260d75794dac171297919f4aa403fb8
Dengke Liu <dengkeliu92@gmail.com>
2021-08-09 10:02:35 -0700

    Update [Hard]ShortedSubarraySumAtLeastK.java

--- [Hard]ShortedSubarraySumAtLeastK.java
+++ [Hard]ShortedSubarraySumAtLeastK.java
@@ -22,5 +22,20 @@
 //    Still need to enumerate the start and end of the subarray. There are N^2 combinations
 //    O(N^2)
 // 3. What algorithms are faster than O(N^2)?
-//    NlogN -> 60% n 次 LogN logN -> binary search, or heap
-//             20% logN 次 N 次操作
+//    NlogN -> 60% n 次 LogN operation logN -> binary search, or heap, segment tree
+//             20% logN 次 N 次操作 use O(N) time to check the answer
+//    The solutuon to this problem definitely has an range, from length 1 to N (or -1 if not found)
+
+
+// Binary search on the solution set
+//                       find the shortest subarrary with SUM >= 4
+// array elements           4     | -2   |  3    |  -4  |  5   | -6   |
+// subarray length          0     |  1   |  2    |  3   |  4   |  5   | 6
+// sum>= 4 and length == x  false | true | false | false| false| true | false
+// sum>= 4 and length <= x  false | true | true  | true | true | true | true
+
+// for binary search, we are looking for OOO|XXX pattern 
+// so we know whether to choose right or left
+// so we want to do sum>=K and length <= x
+//
+
