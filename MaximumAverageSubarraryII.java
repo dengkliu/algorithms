f38f62e7798f00083ada448ba977067675f0a7a8
Dengke Liu <dengkeliu92@gmail.com>
2021-08-10 20:56:07 -0700

    Update MaximumAverageSubarraryII.java

---
+++ MaximumAverageSubarraryII.java
@@ -0,0 +1,3 @@
+// https://www.lintcode.com/problem/617/
+// Given an array with positive and negative numbers, 
+// find the maximum average subarray which length should be greater or equal to given length k.
