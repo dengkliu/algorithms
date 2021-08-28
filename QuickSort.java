063de9fea12d26f756b15f2cf4726f0f9b628ce1
Dengke Liu <dengkeliu92@gmail.com>
2021-08-27 21:49:53 -0400

    Create QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,44 @@
+public class Solution {
+
+	public void sort(int[] A) {
+
+		quickSort(A, 0, A.length -1);
+
+		return;
+
+	}
+
+	void quickSort(int[] A, int start, int end) {
+
+		if (start >= end) {
+			return;
+		}
+
+		int mid = (start + end)/2;
+
+		int pivot = A[mid];
+
+		int left = start, right = end;
+
+		while (left <= right) {
+
+			while (left <=right && A[left] < pivot) {
+				left ++;
+			}
+
+			while (left <= right && A[right] > pivot) {
+				right --;
+			}
+
+			if (left <= right) {
+				int temp = A[left];
+				A[left] = A[right];
+				A[right] = left;
+			}
+		}
+
+		quickSort(A, start, left);
+		quickSort(A, right, end);
+	}
+
+}
