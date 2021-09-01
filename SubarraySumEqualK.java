9f10b87d5582390fc8c9c0ad4652d05cf8cfbd44
Dengke Liu <dengkeliu92@gmail.com>
2021-08-31 20:20:55 -0400

    Change

--- SubarraySumEqualK.java
+++ SubarraySumEqualK.java
@@ -1,77 +1,63 @@
-
-// https://www.lintcode.com/problem/1844/
+// https://www.lintcode.com/problem/838/
+// Subarray Sum Equals K
 // Given an array of integers and an integer k, 
-// you need to find the minimum size of continuous no-empty subarrays whose sum equals to k, and return its length.
-// if there are no such subarray, return -1.
+// you need to find the total number of continuous subarrays whose sum equals to k.
 
-// Input: nums = [1,1,1,2] and k = 3
-// Output: 2 = 1 + 2
+// Input: nums = [1,1,1] and k = 2
+// Output: 2
+// Explanation:
+// subarray [0,1] and [1,2]
 
-// 1. Brute Force - enumerate the start and end of the subarray, 
-//    calculate the sum for each of them, and check if it is K。 It is going to be O(N^3)
-//    for (int start = 0; start < n; start ++) {
-//        for (int end = start + 1; end < n; end ++) {
-//             for (int k = start; k < end; k ++) {
-//             }
-//        }
-//    }
-// 2. Calculate Prefix Sum beforehand. Don't need to calculate the sum of subarray again.
-//    Still need to enumerate the start and end of the subarray. There are N^2 combinations
-//    O(N^2)
-// 3. Let's think in this way. We just need to enumerate the end. The start must be at the index
-//    which make sure that prefixSum[end] - prefixSum[start -1] is the K. We don't have length constraints here, so the start 
-//    can be from 0 to end - 1. We can keep tracking the subarray length.
-//    How do we find prefixSum[start-1] in O(1) time? We can use hashmap. Hashmap checks key existance in O(1) time
-//    So we can have a hashmap with sum to index pair. 
-//    We enumerate the end from index 0 to n-1, for each one we look up the hashmap. 
-//    This can be done in O(N)
+// Brute force - enumerate O(N^3)
+// Use prefixSum and enumerate the end, use hashmap to store sum to index pair
+// One sum can map to multiple indexes
+
 
 public class Solution {
-    
-    /*
+    /**
      * @param nums: a list of integer
      * @param k: an integer
-     * @return: return an integer, denote the minimum length of continuous subarrays whose sum equals to k
+     * @return: return an integer, denote the number of continuous subarrays whose sum equals to k
      */
-
-    // [1, 1, 1]
-    // [-1 -> 0, ]
-
-    public int subarraySumEqualsKII(int[] nums, int k) {
-
+    public int subarraySumEqualsK(int[] nums, int k) {
+        // write your code here
         if (nums == null || nums.length == 0) {
-            return -1;
+            return 0;
         }
 
         int n = nums.length;
-        
-        // 这里我们选择优化 因为我们有hashmap存放prefix sum
-        // 我们可以不用数组了
-        long prefixSum = 0;
+
+        // prefixSum[i] = A[0] + ... + A[i-1];
+        int[] prefixSum = new int[n+1];
 
         // This should be sum to index
-        Map<Long, Integer> sumToIndexMap = new HashMap<>();
+        Map<Integer, Set<Integer>> sumToIndex = new HashMap<>();
 
+        prefixSum[0] = 0;
         // 默认为初始Index为-1
-        sumToIndex.put((long)0, -1);
+        Set<Integer> indexSet = new HashSet<>();
+        indexSet.add(-1);
+        sumToIndex.put(0, indexSet);
 
-        // 准备打擂台
-        int result = Integer.MAX_VALUE;
+        int result = 0;
 
-        for (int i = 0; i < nums.length; i ++) {
-            
-            prefixSum = prefixSum + nums[i];
+        for (int i = 1; i < n + 1; i ++) {
 
-            if(sumToIndexMap.containsKey(prefixSum - k)) {
-                // We find A[0] + .. + A[start] is prefixSum[i] - K
-                // Therefore A[start + 1] + ... A[i -1] is K
-                int start = sumToIndex.get(prefixSum - k);
-                result = Math.min(result, i - start);
+            // now the end is i - 1 A[0] + ... + A[i-1]
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
+
+            Set<Integer> set = sumToIndex.containsKey(prefixSum[i]) ? 
+            sumToIndex.get(prefixSum[i]) : new HashSet<>();
+
+            set.add(i - 1);
+
+            if(sumToIndex.containsKey(prefixSum[i] - k)) {
+                result = result + sumToIndex.get(prefixSum[i] - k).size();
             }
 
-            sumToIndex.put(prefixSum, i);
+            sumToIndex.put(prefixSum[i], set);
         }
 
-        return result == Integer.MAX_VALUE ? -1 : result;
+        return result;
     }
 }
