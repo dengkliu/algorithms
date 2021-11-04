b32dc352347dd5d083da4c8b663f7cabc15e8324
Dengke Liu <dengkeliu92@gmail.com>
2021-11-04 19:17:29 -0400

    Create binary_tree_divide_and_conquer.py

---
+++ binary_tree_divide_and_conquer.py
@@ -0,0 +1,17 @@
+
+class Solution:
+	def divide_conquer(root):
+
+		# 递归出口
+		# 一般是看这颗树是不是null就行了
+		if root is None:
+			return
+
+
+		left_result = divide_conquer(root.left)
+
+		right_result = divide_conquer(root.right)
+
+		result = merge left_result and right_result
+
+		return result
