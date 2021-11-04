17a84f2badd685280ab7f7f73ab7f0a29b63f406
Dengke Liu <dengkeliu92@gmail.com>
2021-11-04 15:27:15 -0400

    Update binary_search_template.py

--- binary_search_template.py
+++ binary_search_template.py
@@ -7,10 +7,12 @@ class binarySearch:
 			return -1
 
 		start, end = 0, len(nums) - 1
-
+		
+		# start + 1 < end, to avoid infinite while loops
 		while start + 1 < end:
+			# get the floor
 			mid = (start + end)//2
-
+			
 			if nums[mid] < target:
 				start = mid
 			elif nums[mid] == target:
