efd1179ef6853f3cf731401f568f41d4c1b4e0ad
Dengke Liu <dengkeliu92@gmail.com>
2021-11-10 21:07:52 -0500

    Update FlattenBinaryTreeToLinkedList.java

--- FlattenBinaryTreeToLinkedList.java
+++ FlattenBinaryTreeToLinkedList.java
@@ -1,3 +1,4 @@
+// https://www.lintcode.com/problem/453/
 // Flatten a binary tree to a fake "linked list" in pre-order traversal.
 // Here we use the right pointer in TreeNode as the next pointer in ListNode.
 
