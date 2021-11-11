1addadbaf72c417880ee473a18a4fef96f81564f
Dengke Liu <dengkeliu92@gmail.com>
2021-11-10 21:27:02 -0500

    Create flatten_binary_tree_to_linkedlist.py

---
+++ flatten_binary_tree_to_linkedlist.py
@@ -0,0 +1,66 @@
+# https://www.lintcode.com/problem/453/
+# Flatten a binary tree to a fake "linked list" in pre-order traversal.
+# Here we use the right pointer in TreeNode as the next pointer in ListNode.
+
+# Don't forget to mark the left child of each node to null. 
+# Or you will get Time Limit Exceeded or Memory Limit Exceeded.
+
+# Input:{1,2,5,3,4,#,6}
+# Output：{1,#,2,#,3,#,4,#,5,#,6}
+# Explanation：
+#     1
+#    / \
+#   2   5
+#  / \   \
+# 3   4   6
+
+#1
+# \
+#  2
+#   \
+#    3
+#     \
+#      4
+#       \
+#        5
+#         \
+#          6
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        if root is None:
+            return None
+        
+        stack = []
+        result = []
+
+        stack.append(root)
+
+        # [1 2]
+        # [5 2]
+        # [5 4 3]
+        while stack:
+            node = stack.pop()
+            result.append(node)
+            if len(result) >= 2:
+                prevNode = result[len(result) - 2]
+                prevNode.right = node
+                prevNode.left = None
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
+
+        return result[0]
