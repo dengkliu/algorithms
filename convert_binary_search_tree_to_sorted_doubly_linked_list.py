0194930eef93f43c73fe7bb5928c9a238c53c438
Dengke Liu <dengkeliu92@gmail.com>
2021-11-10 21:08:58 -0500

    Update convert_binary_search_tree_to_sorted_doubly_linked_list.py

--- convert_binary_search_tree_to_sorted_doubly_linked_list.py
+++ convert_binary_search_tree_to_sorted_doubly_linked_list.py
@@ -1,3 +1,22 @@
+# https://www.lintcode.com/problem/1534
+
+# Convert a BST to a sorted circular doubly-linked list in-place. 
+# Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
+
+# We want to transform this BST into a circular doubly linked list. Each node in a doubly linked list has a predecessor and successor. For a circular doubly linked list, 
+# the predecessor of the first element is the last element, and the successor of the last element is the first element
+
+# Input: {4,2,5,1,3}
+#        4
+#       /  \
+#      2    5
+#     / \
+#    1   3
+# Output: "left:1->5->4->3->2  right:1->2->3->4->5"
+# Explanation:
+# Left: reverse output
+# Right: positive sequence output
+
 """
 Definition of TreeNode:
 class TreeNode:
