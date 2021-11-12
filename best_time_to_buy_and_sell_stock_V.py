f6cf4ac008dbdbdf9b19c8b3d5f8557a3fd2f26f
Dengke Liu <dengkeliu92@gmail.com>
2021-11-12 13:35:21 -0500

    Create best_time_to_buy_and_sell_stock_V.py

---
+++ best_time_to_buy_and_sell_stock_V.py
@@ -0,0 +1,44 @@
+# https://www.lintcode.com/problem/1691/
+
+# Given a stock n-day price, you can only trade at most once a day, 
+# you can choose to buy a stock or sell a stock or give up the trade today, 
+# output the maximum profit can be achieved.
+
+# You can engage in mulitple transactions at the same time
+
+# Given `a = [1,2,10,9]`, return `16`
+# Input:
+# [1,2,10,9]
+# Output:
+# 16 -- buy 1, buy 2, sell at 10, sell at 9
+
+# The difference between this problem and problem II is that, you can buy at the second day!!!
+# So you have 2 transactions at hand at the same time
+# How to achieve this?
+# In addition to the solution of problem II (always do a transaction when there is gain)
+# you also want to buy at any given time, check if later you can sell it at higher price
+# Each time you sell, you are selling the previous minimum.
+# You need a data structure to store and update the minimum - priority queue
+
+import heapq
+
+class Solution:
+    """
+    @param a: the array a
+    @return: return the maximum profit
+    """
+    def getAns(self, a):
+        # write your code here
+        heap = []
+        ans = 0
+        for i in a:
+            if heap and i > heap[0]:
+            	# Whenever you sell, you pop the previous minimum, by this you get the solution for problem II
+                ans += i - heapq.heappop(heap)
+                # This doesn't count as buying, it is because you already sell it, and you buy it back, to pretend
+                # not doing anything that day
+                # Later if you found higher price, you can sell it again!
+                heapq.heappush(heap, i)
+            # !!!! Important, this is actually buying
+            heapq.heappush(heap,i)
+        return ans
