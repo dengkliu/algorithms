94822e362c142a9cea97221755bc4cca5993bd41
Dengke Liu <dengkeliu92@gmail.com>
2022-01-04 16:31:47 -0500

    Update binary_search_template.py

--- binary_search_template.py
+++ binary_search_template.py
@@ -1,5 +1,6 @@
 # O(logN) time complexity
 
+# Get the first occurence of the target
 class binarySearch:
 
 	def binary_search(self, nums, target):
@@ -32,3 +33,39 @@ class binarySearch:
 			return end
 
 		return -1
+
+
+# Get the last occurence of the target
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def lastPosition(self, nums, target):
+        
+        # Empty or None will be considered false in boolean context
+        if not nums:
+            return -1
+
+        start, end = 0, len(nums) - 1
+
+        while start + 1 < end:
+            mid = (start + end)//2
+
+            if nums[mid] < target:
+                start = mid
+            elif nums[mid] == target:
+                # move start close to the end, choose the right half
+                start = mid
+            else:
+                end = mid
+        
+        # First check the end
+        if nums[end] == target:
+            return end
+        if nums[start] == target:
+            return start
+        
+        return -1
+
