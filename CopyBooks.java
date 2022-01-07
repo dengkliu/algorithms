b2127c670c5329c6cc495fc444275e08f071df50
Dengke Liu <dengkeliu92@gmail.com>
2022-01-07 17:08:49 -0500

    Update CopyBooks.java

--- CopyBooks.java
+++ CopyBooks.java
@@ -1,3 +1,4 @@
+# https://www.lintcode.com/problem/437/
 // Given n books and the i-th book has pages[i] pages. There are k persons to copy these books.
 // These books list in a row and each person can claim a continous range of books. 
 // For example, one copier can copy the books from i-th to j-th continously, but he can not copy the 1st book, 2nd book and 4th book (without 3rd book).
