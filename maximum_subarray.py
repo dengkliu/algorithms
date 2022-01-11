d8db3cfb73ce55e66c28b69b81e678779a77a678
Dengke Liu <dengkeliu92@gmail.com>
2022-01-11 14:52:16 -0500

    Update maximum_subarray.py

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -27,44 +27,29 @@
 #    be found while enumerate the end, and it is O(1) time to fetch it later
 #    Enumerating the end can be done in O(N)
 
-public class Solution {
-    /**
-     * @param nums: A list of integers
-     * @return: A integer indicate the sum of max subarray
-     */
-    public int maxSubArray(int[] nums) {
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A integer indicate the sum of max subarray
+    """
+    def maxSubArray(self, nums):
 
-    	// Edge case check
-    	if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        if not nums:
+            return 0
 
-        int n = nums.length;
+        # write your code here
+        prefix_sum = 0
+        min_prev_sum = 0 # 初始为0！一个元素都还没有的时候
+        result = float('-inf')
 
-        // prefix[i] - the sum of first i numbers in the array
-        int[] prefixSum = new int[n+1];
-        prefixSum[0] = 0; 
+        # -2, 2, -3, 4, -1, 2, 1, -5, 3
+        # -2, 0, -3, 1
+        # inf, 2, 2, 3,
+        # inf, -2, -2, -2  
 
-        // Tracking the minimum prefix sum so far. Initialized as 0 (the first 0 number sum)
-        int minPrefixSum = 0;
-
-        // 准备好打擂台
-        int result = Integer.MIN_VALUE;
-
-        // Starting from adding 1 number
-        for (int i = 1; i < n + 1; i ++) {
-            prefixSum[i] = prefixSum[i-1] + nums[i-1];
-
-            // Now get the maximum subarray ending in this number
-            // by substracting the previous minimum prefix sum
-            result = Math.max(result, prefixSum[i] - minPrefixSum);
-            
-            // Updating the minimum prefix sum
-            minPrefixSum = Math.min(minPrefixSum, prefixSum[i]);
-        }
-
-        return result;
-
-    }
-}
-
+        for num in nums:
+            prefix_sum = prefix_sum + num
+            result = max(result, prefix_sum - min_prev_sum)
+            min_prev_sum = min(min_prev_sum, prefix_sum)
+        
+        return result
