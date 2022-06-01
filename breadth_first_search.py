b92f15bbf56f5e29e32f5b6dc23123c90bb8d599
Dengke Liu <dengkeliu92@gmail.com>
2022-06-01 14:02:50 -0400

    Update breadth_first_search.py

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,32 +1,32 @@
 # Python 普通BSF模板
 
-// ArrayDeque is faster than linkedlist for adding elements
-// Queue to store nodes while searching
+# ArrayDeque is faster than linkedlist for adding elements
+# Queue to store nodes while searching
 queue = collections.deque()
-// Set to store visited nodes
+# Set to store visited nodes
 visited = set()
 
-// 初始点入队
+# 初始点入队
 queue.append(0);
-// 在入队的同时加到visited
+# 在入队的同时加到visited
 visited.add(0)；
 
 while queue:
 
-    // 弹出第一个，先入队先出队
+    #弹出第一个，先入队先出队
 	now = queue.popleft();
 
 	for next in self.__findNext(now):
 
-		// 已经在visited里面
-		// 在界外
-		// 其他不满足条件的，直接跳过
+		# 已经在visited里面
+		# 在界外
+		# 其他不满足条件的，直接跳过
 		if not self.__is_valid(now):
 			continue
 
 		queue.append(next);
-		// 一定在入队的时候就要加入visited
-		// 否则会重复入队
+		# 一定在入队的时候就要加入visited
+		# 否则会重复入队
 		visited.add(next);
 
 
@@ -37,17 +37,17 @@ distance = 0;
 
 while queue:
 
-	// 先取这一层的size
-	// queue.size()每次调用都会显示实时的size
+	# 先取这一层的size
+	# queue.size()每次调用都会显示实时的size
 	queueSize = len(queue)
 
 	for i in range(queueSize):
 		now = queue.popleft();
 
 		for next in self.__find_next(now):
-			// 已经在visited里面
-			// 在界外
-			// 其他
+			# 已经在visited里面
+			# 在界外
+			# 其他
 			if not self.__isvalid(next):
 				continue
 
