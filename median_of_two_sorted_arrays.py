05acbb4e11d01728d9cec2b3a72334f44df7db42
Dengke Liu <dengkeliu92@gmail.com>
2023-10-13 20:57:13 -0400

    Update median_of_two_sorted_arrays.py

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -1,4 +1,4 @@
-# https://www.lintcode.com/problem/65/description
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
 # There are two sorted arrays A and B of size m and n respectively. 
 # Find the median of the two sorted arrays.
 # The overall run time complexity should be O(log(m + n)).
