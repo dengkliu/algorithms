f7356c087754cf9409eb9555751d97b20d79ef81
dengke <dengke@amazon.com>
2024-01-16 17:17:38 -0500

    Create post_order_traversal_bst.py

---
+++ post_order_traversal_bst.py
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        
+        result = []
+
+        self.postorder(root, result)
+
+        return result
+
+    def postorder(self, root, result):
+        if root.left:
+            self.postorder(root.left, result)
+        if root.right:
+            self.postorder(root.right, result)
+        result.append(root.val)
