30e061bd55dbcb6ca083df1c48cd01fa7b056725
Dengke Liu <dengkeliu92@gmail.com>
2024-01-17 17:12:04 -0500

    Update flatten_binary_tree_to_linkedlist.py

--- flatten_binary_tree_to_linkedlist.py
+++ flatten_binary_tree_to_linkedlist.py
@@ -1,4 +1,5 @@
-# https://www.lintcode.com/problem/453/
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+
 # Flatten a binary tree to a fake "linked list" in pre-order traversal.
 # Here we use the right pointer in TreeNode as the next pointer in ListNode.
 
