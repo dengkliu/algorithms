810e5db72587be7f0caca0a8447379d7eb470497
Dengke Liu <dengkeliu92@gmail.com>
2024-01-18 16:30:15 -0500

    Update binary_tree_lowest_common_ancestor.py

--- binary_tree_lowest_common_ancestor.py
+++ binary_tree_lowest_common_ancestor.py
@@ -20,14 +20,31 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        if p == root or q == root:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None or root == p or root == q:
             return root
         
-        
-        l = self.lowestCommonAncestor(root.left, p, q) if root.left else None
-        r = self.lowestCommonAncestor(root.right, p, q) if root.right else None
+        l = self.lowestCommonAncestor(root.left, p, q) 
+        r = self.lowestCommonAncestor(root.right, p, q)
 
+        # one node found on the left and the other found on the right
         if l and r:
             return root
-        
-        return l or r 
+
+        # one node found the left or right, and None found from the other side
+        # can just return because the other node must be the children of this node
+        return l or r
