6d4fa5072e3bb5768b3fbb5e7c6647884e6875a7
Dengke Liu <dengkeliu92@gmail.com>
2024-01-28 19:12:47 -0500

    Update serialize_and_deserialize_n_ary_tree.py

--- serialize_and_deserialize_n_ary_tree.py
+++ serialize_and_deserialize_n_ary_tree.py
@@ -1,6 +1,5 @@
 # https://leetcode.com/problems/serialize-and-deserialize-n-ary-tree/
 
-
 # Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
 
 # Design an algorithm to serialize and deserialize an N-ary tree. An N-ary tree is a rooted tree in which each node has no more than N children. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that an N-ary tree can be serialized to a string and this string can be deserialized to the original tree structure.
