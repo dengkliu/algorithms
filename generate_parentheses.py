7e04804246fb69c8dcad13204446ea3ff9a7f009
Dengke Liu <dengkeliu92@gmail.com>
2024-03-27 16:41:19 -0400

    Update generate_parentheses.py

--- generate_parentheses.py
+++ generate_parentheses.py
@@ -1,32 +1,32 @@
-// https://leetcode.com/problems/generate-parentheses/
+# https://leetcode.com/problems/generate-parentheses/
 
-// Given n, and there are n pairs of parentheses, write a function to generate all combinations of well-formed parentheses, 
-// And return the combination result.
+# Given n, and there are n pairs of parentheses, write a function to generate all combinations of well-formed parentheses, 
+# And return the combination result.
 
-// Input: 3
-// Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+# Input: 3
+# Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
 
-// 典型的DFS问题
-// 从上一个推下一个
-// 对于每一个pre 可以生成 （pre） pre() 和 ()pre
-// 但是要求pre本身是一个valid的parenthese
-// 会漏掉比如 (())(()) 这种情况 因为 ())(() 不是一个valid parenthese
+# 典型的DFS问题
+# 从上一个推下一个
+# 对于每一个pre 可以生成 （pre） pre() 和 ()pre
+# 但是要求pre本身是一个valid的parenthese
+# 会漏掉比如 (())(()) 这种情况 因为 ())(() 不是一个valid parenthese
 
-// 因此搜素树变成 一个个加左括号或者右括号
-// 在每一个节点 规则是 左括号不能超过总括号数的一半 右括号数不能超过左括号数
-// 还需要一个参数记录目前已经加了多少括号了
-// 如果到了2*n 就得到了一个解 返回
+# 因此搜素树变成 一个个加左括号或者右括号
+# 在每一个节点 规则是 左括号不能超过总括号数的一半 右括号数不能超过左括号数
+# 还需要一个参数记录目前已经加了多少括号了
+# 如果到了2*n 就得到了一个解 返回
 
-// 假设 n = 2
-//             ""
-//             /\
-//            (  )
-//           / \  \ 
-//          (( ()  *
-//         / \  \
-//        * (() ()(
-//           |   |
-//          (()) ()()
+# 假设 n = 2
+#             ""
+#             /\
+#            (  )
+#           / \  \ 
+#          (( ()  *
+#         / \  \
+#        * (() ()(
+#           |   |
+#          (()) ()()
 
 class Solution(object):
     def generateParenthesis(self, n):
