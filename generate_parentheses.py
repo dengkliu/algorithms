f1dc92c372998f699538b50a860f909d1e4241c9
Dengke Liu <dengkeliu92@gmail.com>
2024-03-27 17:45:52 -0400

    Update generate_parentheses.py

--- generate_parentheses.py
+++ generate_parentheses.py
@@ -60,3 +60,23 @@ class Solution(object):
         
         self._dfs(curr + "(", open_cnt + 1, close_cnt, total_cnt + 1, n, result)
         self._dfs(curr + ")", open_cnt, close_cnt + 1, total_cnt + 1, n, result)
+
+
+# backtracking
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        answer = []
+        def backtracking(cur_string, left_count, right_count):
+            if len(cur_string) == 2 * n:
+                answer.append("".join(cur_string))
+                return
+            if left_count < n:
+                cur_string.append("(")
+                backtracking(cur_string, left_count + 1, right_count)
+                cur_string.pop()
+            if right_count < left_count:
+                cur_string.append(")")
+                backtracking(cur_string, left_count, right_count + 1)
+                cur_string.pop()
+        backtracking([], 0, 0)
+        return answer
