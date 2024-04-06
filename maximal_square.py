b6d17ae7bfbf97f0f6ad97322e1894335d0d7607
Dengke Liu <dengkeliu92@gmail.com>
2024-04-06 19:25:16 -0400

    Update maximal_square.py

--- maximal_square.py
+++ maximal_square.py
@@ -1,4 +1,4 @@
-# https://www.lintcode.com/problem/436/
+# https://leetcode.com/problems/maximal-square/description/
 
 # Given a 2D binary matrix filled with 0's and 1's, 
 # find the largest square containing all 1's and return its area.
