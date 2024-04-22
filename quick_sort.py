9d6f9c96a9c380232a79924fa9cfb952c6bab2ad
Dengke Liu <dengkeliu92@gmail.com>
2024-04-22 19:33:18 -0400

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,33 +1,24 @@
 class Solution:
-
+	
 	def sort(self, A):
 		self.quick_sort(A, 0, len(A) - 1)
-
-    
-    # start - the start index
-    # end - the end index
-	def quick_sort(self, A, start, end):
-
+		
+# start - the start index
+# end - the end index
+    def quickSort(self, A, start, end):
 		if start >= end:
 			return
-
 		left, right = start, end
-
 		pivot = A[(left + right) // 2]
-
-		while left <= right:
-			while left <= right and A[left] < pivot:
+		while left + 1 < right:
+			while left + 1 < right and A[left] < pivot:
 				left += 1
-
-			while left <= right and A[right] > pivot:
+			while left + 1 < right and A[right] > pivot:
 				right -= 1
-
-			if left <= right:
+			if left + 1 < right:
 				A[left], A[right] = A[right], A[left]
 				left += 1
 				right -=1
-
-
-		self.quick_sort(start, left)
-		self.quick_sort(right, end)
-
+		
+		self.quickSort(A, start, left)
+		self.quickSort(A, right, end)
