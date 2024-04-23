885f350966b12d1cc06cfb6ac5cc920e5237249c
Dengke Liu <dengkeliu92@gmail.com>
2024-04-22 20:47:57 -0400

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -11,50 +11,42 @@ class Solution:
 
 
     # [2, 1]
+def mergeSort(A, start, end):
+  if start >= end:
+    return
+  
+  mid = (start + end) // 2
+  mergeSort(A, start, mid)
+  mergeSort(A, mid + 1, end)
+  merge(A, start, mid, end)
 
-	def merge_sort(self, A, start, end, sorted_A):
-
-		if start >= end:
-			return
-
-		mid = (start + end) // 2
-		# handle from start to mid
-		self.merge_sort(start, mid)
-		# handle from mid + 1 to end
-		self.merge_sort(mid + 1, end)
-		
-		self.merge(A, start, end, sorted_A)
-
-
-	def merge(self, A, start, end, sorted_A):
-
-		mid = (start + end) // 2
-
-		left, right = start, mid + 1
-
-		index = start
-
-		while left <= mid and right <= end:
-			if A[left] > A[right]:
-				sorted_A[index] = A[left]
-				index += 1
-				left += 1
-			else:
-				sorted_A[index] = A[right]
-				index += 1
-				right += 1
-
-
-		while left <= mid:
-			sorted_A[index] = A[left]
-			left ++
-
-		while right <= end:
-			sorted_A[index] = A[right]
-			right ++
-
-		for i in range(start, end + 1):
-			A[i] = sorted_A[i]
+def merge(A, start, mid, end):
+  sorted_A = [0] * (end - start + 1)
+  left, right = start, mid + 1
+  index = 0
+  while left <= mid and right <= end and index <= end - start:
+    if start == 3 and end == 5:
+      print(A[left])
+      print(A[right])
+    if A[left] < A[right]:
+      sorted_A[index] = A[left]
+      index += 1
+      left += 1
+    else:
+      sorted_A[index] = A[right]
+      index += 1
+      right += 1	
+  while left <= mid and index <= end - start:
+    sorted_A[index] = A[left]
+    left += 1
+    index += 1
+  while right <= end and index <= end - start:
+    sorted_A[index] = A[right]
+    right += 1
+    index += 1
+  # copy to original array
+  for i in range(start, end + 1):
+    A[i] = sorted_A[i - start]
 
 
 
