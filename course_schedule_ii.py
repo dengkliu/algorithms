317af1c17a5f5e7488ae3d98d3c7a59757bb21df
Dengke Liu <dengkeliu92@gmail.com>
2024-05-02 13:06:55 -0400

    Update course_schedule_ii.py

--- course_schedule_ii.py
+++ course_schedule_ii.py
@@ -6,7 +6,6 @@
 # For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 # Return the ordering of courses you should take to finish all courses. If there are many valid answers, return # any of them. If it is impossible to finish all courses, return an empty array.
 
-
 class Solution:
     """
     @param num_courses: a total of n courses
@@ -15,14 +14,16 @@ class Solution:
     """
     def findOrder(self, num_courses, prerequisites):
 
-        course_to_indegrees = {i:0 for i in range(num_courses)}
-        course_to_courses = {i:[] for i in range(num_courses)}
+        indegrees = {i:0 for i in range(num_courses)}
+        graph = collections.defaultdict(list)
 
-        for prerequisite in prerequisites:
-            course_to_courses[prerequisite[1]].append(prerequisite[0])
-            course_to_indegrees[prerequisite[0]] +=1
+        for u, v in prerequisites:
+            # here v is a child of u
+            graph[v].append(u)
+            # increase the child indegreee by 1
+            indegrees[u] += 1
 
-        start_courses = [course for course in course_to_indegrees if course_to_indegrees[course] == 0]
+        start_courses = [course for course in indegrees if indegrees[course] == 0]
         queue = collections.deque(start_courses)
         courses = []
 
@@ -30,12 +31,11 @@ class Solution:
             current_course = queue.popleft()
             courses.append(current_course)
 
-            for next_course in course_to_courses[current_course]:
-                course_to_indegrees[next_course] -= 1
-                if course_to_indegrees[next_course] == 0:
+            for next_course in graph[current_course]:
+                indegrees[next_course] -= 1
+                if indegrees[next_course] == 0:
                     queue.append(next_course)
 
-        
         if len(courses) == num_courses:
             return courses
         
