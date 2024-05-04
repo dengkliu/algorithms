e49b9576d0999ea9d5bb1827d816b367bfb488a8
dengke <dengke@amazon.com>
2024-05-04 12:33:41 -0400

    Update 01_matrix.py

--- 01_matrix.py
+++ 01_matrix.py
@@ -41,3 +41,38 @@ class Solution(object):
                     
         
         return result
+
+
+# dynamic programming
+class Solution(object):
+    def updateMatrix(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        dp = [row[:] for row in mat]
+        m, n = len(dp), len(dp[0])
+
+        for row in range(m):
+            for col in range(n):
+                min_neighbor = float('inf')
+                if dp[row][col] != 0:
+                    if row > 0:
+                        min_neighbor = min(min_neighbor, dp[row - 1][col])
+                    if col > 0:
+                        min_neighbor = min(min_neighbor, dp[row][col - 1])
+                        
+                    dp[row][col] = min_neighbor + 1
+    
+        for row in range(m - 1, -1, -1):
+            for col in range(n - 1, -1, -1):
+                min_neighbor = float('inf')
+                if dp[row][col] != 0:
+                    if row < m - 1:
+                        min_neighbor = min(min_neighbor, dp[row + 1][col])
+                    if col < n - 1:
+                        min_neighbor = min(min_neighbor, dp[row][col + 1])
+                        
+                    dp[row][col] = min(dp[row][col], min_neighbor + 1)
+
+        return dp
