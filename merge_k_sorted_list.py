9a628b41fc517bb8292fa462b9161a761e35591f
dengke <dengke@amazon.com>
2024-05-06 22:45:00 -0400

    Update merge_k_sorted_list.py

--- merge_k_sorted_list.py
+++ merge_k_sorted_list.py
@@ -24,10 +24,12 @@ class Solution(object):
             for index in range(0, len(lists) - interval, interval * 2):
                 list1 = lists[index]
                 list2 = lists[index + interval]
+                # why do we assign to lists[index]
                 lists[index] = self._merge_two_lists(list1, list2)
             
             interval = interval * 2
         
+        # eventually all goes to lists[0]
         return lists[0]
 
 
@@ -39,24 +41,24 @@ class Solution(object):
         """
         head = ListNode()
         # why do we need a dummy
-        dummy = head
+        node = head
 
         if not list1 and not list2:
             return None
 
         while list1 and list2:
             if list1.val < list2.val:
-                dummy.next = list1
+                node.next = list1
                 list1 = list1.next
-                dummy = dummy.next
+                
             else:
-                dummy.next = list2
+                node.next = list2
                 list2 = list2.next
-                dummy = dummy.next
+            node = node.next
 
         if list1:
-            dummy.next = list1
+            node.next = list1
         else:
-            dummy.next = list2
+            node.next = list2
 
         return head.next
