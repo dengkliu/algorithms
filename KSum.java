0de00eb0af0ebfb8a77bbd63baa24d1959bdfd48
Dengke Liu <dengkeliu92@gmail.com>
2024-05-08 17:55:35 -0400

    Update KSum.java

--- KSum.java
+++ KSum.java
@@ -1,4 +1,4 @@
-// https://www.lintcode.com/problem/89/
+// https://leetcode.com/problems/combination-sum-iv/
 
 // Given n distinct positive integers, integer k (k=n) and a number target.
 // Find k numbers where sum is target. Calculate how many solutions there are?
