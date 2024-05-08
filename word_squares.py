9a6139bdefbce94fc9ca7e802c9de20c32a7dffa
dengke <dengke@amazon.com>
2024-05-08 13:32:54 -0400

    Update word_squares.py

--- word_squares.py
+++ word_squares.py
@@ -19,66 +19,51 @@
 # 2. 怎么优化呢？第n行的word 前n-1个字母一定与第n列的前n-1个字母相同 ---> 前缀 prefix问题！
 #    如何快速找到以某个前缀为开头的单词？1. trie, prefix tree 2. hashtable
 
-class Solution:
-    """
-    @param words: a set of words without duplicates
-    @return: all word squares
-    """
+class Solution(object):
     def wordSquares(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[str]]
+        """
 
-        prefix_to_words = self.get_prefix_to_words(words)
+        def get_prefix_to_words(words):
+            prefix_to_words = collections.defaultdict(list)
 
-        # 定义储存结果的list
+
+            # O(N * L)
+            for word in words:
+                for i in range(len(word)):
+                    prefix = word[0:i + 1]
+                    prefix_to_words[prefix].append(word)
+            
+            return prefix_to_words
+
+        def search(square, squares, prefix_to_words):
+            if len(square) == len(square[0]):
+                # why do we want to copy the square?
+                squares.append(list(square))
+                return
+            
+            cur_row_cnt = len(square)
+
+            # This is for optimization
+            for r in range(cur_row_cnt, len(square[0])):
+                prefix = ''.join([square[i][r] for i in range(cur_row_cnt)])
+                if prefix not in prefix_to_words:
+                    return
+            
+            prefix = ''.join([square[i][cur_row_cnt] for i in range(cur_row_cnt)])
+
+            for word in prefix_to_words[prefix]:
+                square.append(word)
+                search(square, squares, prefix_to_words)
+                square.pop()
+
+        prefix_to_words = get_prefix_to_words(words)
+        
         squares = []
 
         for word in words:
-            self.search(prefix_to_words, [word], squares)
+            search([word], squares, prefix_to_words)
 
         return squares
-
-    def get_prefix_to_words(self, words) :
-
-        # dictionary
-        prefix_to_words = {} 
-
-        for word in words:
-            for i in range(len(word)):
-                # get first i characters form the word
-                prefix = word[0:i + 1]
-                # Python dictionary method setdefault() is similar to get(), but will set dict[key]=default if key is not already in dict.
-                prefix_to_words.setdefault(prefix, [])
-                # 加prefix -> word 关系
-                prefix_to_words[prefix].append(word)
-
-        return prefix_to_words
-
-    
-    def search(self, prefix_to_words, square, squares):
-        square_row_cnt = len(square[0])
-        curr_row_cnt = len(square)
-
-        if square_row_cnt == curr_row_cnt:
-            # 重要！需要再造一个list 不然这个square会被之后的代码修改
-            squares.append(list(square))
-            return
-
-        # 可行性剪枝提高效率 通过前两行 其实也可确定下面所有行的前两个字母 看看有没有再说 没有就直接返回了    
-        for row_index in range(curr_row_cnt, square_row_cnt):
-            prefix = ''.join([square[i][row_index] for i in range(curr_row_cnt)])
-            # check key 在不在 dictionary 里
-            if prefix not in prefix_to_words:
-                return            
-
-        # separator: ''
-        # build a list iteratively [ element for element in another list ]    
-        prefix = ''.join([square[i][curr_row_cnt] for i in range(curr_row_cnt)])
-
-        # print('prefix: ' + prefix)
-        
-        for word in prefix_to_words.get(prefix, []):
-            square.append(word)
-            self.search(prefix_to_words, square, squares)
-            # remove last item
-            square.pop() 
-
-    
