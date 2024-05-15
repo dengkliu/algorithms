faaafe95c6176b6b3e121cf9ea15e017f6bf9ebf
dengke <dengke@amazon.com>
2024-05-15 10:25:32 -0400

    Create dot_product_of_2_sparse_vectors.py

---
+++ dot_product_of_2_sparse_vectors.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/dot-product-of-two-sparse-vectors/
+
+# Given two sparse vectors, compute their dot product.
+
+# Implement class SparseVector:
+
+# SparseVector(nums) Initializes the object with the vector nums
+# dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+# A sparse vector is a vector that has mostly zero values, you should store the sparse vector efficiently and compute the dot product between two SparseVector.
+
+# Follow up: What if only one of the vectors is sparse?
+
+
+class SparseVector:
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nonzeros = {}
+
+        for i, n in enumerate(nums):
+            if n != 0:
+                self.nonzeros[i] = n
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec):
+        """
+        :type vec: 'SparseVector'
+        :rtype: int
+        """
+        result = 0
+        for i, n in self.nonzeros.items():
+            if i in vec.nonzeros:
+                result += n * vec.nonzeros[i]
+        return result
+        
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
