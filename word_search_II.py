672b7b4b6ec93b040491e605412f6df2bee0b352
Dengke Liu <dengkeliu92@gmail.com>
2024-05-29 13:19:52 -0400

    Update word_search_II.py

--- word_search_II.py
+++ word_search_II.py
@@ -1,9 +1,8 @@
-# https://www.lintcode.com/problem/132/
+# https://leetcode.com/problems/word-search-ii/
 
-# Given a matrix of lower alphabets and a dictionary. 
-# Find all words in the dictionary that can be found in the matrix. 
-# A word can start from any position in the matrix and go left/right/up/down to the adjacent position. 
-# One character only be used once in one word. No same word in dictionary
+# Given an m x n board of characters and a list of strings words, return all words on the board.
+
+# Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
 
 # Input：["doaf","agai","dcan"]，["dog","dad","dgdg","can","again"]
 # Output：["again","can","dad","dog"]
@@ -13,92 +12,61 @@
 #  d c a n
 # search in Matrix，so return ["again","can","dad","dog"].
 
-# 单词搜索一般用DFS
-# 从方正的每个位置开始 看看能不能走出一个单词 
-# 为了优化搜索速度，可以用trie. 
-# Trie的形状类似树，可以从第一个字符开始逐渐往下，看看能不能找出一个完整的单词
+class Solution(object):
+    def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
 
-# Definition of trie
-class TrieNode:
-    def __init__(self):
-        self.is_word = False
-        self.word = None
-        self.children = {}
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    def get_root(self):
-        return self.root
-    def add(self, word):
-        node = self.get_root()
-        for letter in word:
-            if letter not in node.children:
-                node.children[letter] = TrieNode()
-            node = node.children[letter]
-        node.is_word = True
-        node.word = word
-
-class Solution:
-
-    def __init__(self):
-        self.trie = Trie()
-        # List of tuple, 在方阵中移动小技巧
-        self.DIRECTIONS = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-    """
-    @param board: A list of lists of character
-    @param words: A list of string
-    @return: A list of string
-    """
-    def wordSearchII(self, board, words):
-        # write your code here
-
-        if board == None or len(board) == 0:
+        if not words or len(words) == 0 or len(words[0]) == 0:
             return []
-        
-        for word in words:
-            self.trie.add(word)
 
+        # smart and fast way to build trie!
+        trie = {}
+        WORD_KEY = "$"
+        for word in words:
+            node = trie
+            for letter in word:
+                node = node.setdefault(letter, {})
+            node[WORD_KEY] = word
+
+        n = len(board)
+        m = len(board[0])
+
+        DIRECTIONS = [[0, -1], [0, 1], [-1, 0], [1, 0]]
         result = []
 
-        row_count = len(board)
-        col_count = len(board[0])
-
-        # (M * N) ^ 2 
-        for i in range(row_count):
-            for j in range(col_count):
-                letter = board[i][j]
-                board[i][j] = '*'
-                self.dfs(i, j, letter, board, self.trie.get_root(), result)
-                board[i][j] = letter
-        return result
-
-    def dfs(self, row, col, current, board, node, result):
-
-        if current not in node.children:
-            return
-    
-        if node.children[current].is_word:
-            if node.children[current].word not in result:
-                result.append(node.children[current].word)
-
-        for dx, dy in self.DIRECTIONS:
-            next_row = row + dx
-            next_col = col + dy
-
-            if not self.valid(next_row, next_col, board):
-                continue
+        def dfs(r, c, result, board, n, m, parent):
+            letter = board[r][c]
             
-            next_letter = board[next_row][next_col]
-            board[next_row][next_col] = '*'
-            self.dfs(next_row, next_col, next_letter, board, node.children[current], result)
-            board[next_row][next_col] = next_letter
+            if letter not in parent:
+                return
+            node = parent[letter]
 
-    def valid(self, row, col, board):
-        if row < 0 or row >= len(board):
-            return False
-        if col < 0 or col >= len(board[0]):
-            return False
-        if board[row][col] == '*':
-            return False
-        return True
+            # why do we need this?
+            # to optimize performance, early stop the search
+            if not node:
+                parent.pop(letter)
+                return
 
+            # why do we want to do this?
+            # to de-deduplicate
+            word_match = node.pop(WORD_KEY, False)
+            if word_match:
+                result.append(word_match)
+            
+            board[r][c] = "*"
+            for r_d, c_d in DIRECTIONS:
+                r_n = r + r_d
+                c_n = c + c_d
+                if r_n in range(n) and c_n in range(m):
+                    dfs(r_n, c_n, result, board, n, m, node)
+            board[r][c] = letter
+
+        for r in range(n):
+            for c in range(m):
+                dfs(r, c, result, board, n, m, trie)
+
+        return result
