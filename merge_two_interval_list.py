b140f37a0bc70eacf2c191eac149330bd9506943
dengke <dengke@amazon.com>
2024-06-10 10:16:19 -0400

    Create merge_two_interval_list.py

---
+++ merge_two_interval_list.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/discuss/interview-question/124616/
+
+# Given A and B two interval lists, A has no overlap inside A and B has no overlap inside B. Write the function to merge two interval lists, output the result with no overlap. Ask for a very efficient solution
+
+
+def mergeInterval(self, A, B):
+
+
