7a68207adf19c61ee390e423e6c3c4162ac7a103
dengke <dengke@amazon.com>
2024-06-12 18:58:11 -0400

    Update toeplitz_matrix.py

--- toeplitz_matrix.py
+++ toeplitz_matrix.py
@@ -10,14 +10,26 @@ class Solution(object):
         """
         :type matrix: List[List[int]]
         :rtype: bool
-        """
-        groups = {}
-        
-        for r, row in enumerate(matrix):
-            for c, val in enumerate(row):
-                if r - c not in groups:
-                    groups[r - c] = val
-                elif groups[r - c] != val:
+        """    
+        for r in range(1, len(matrix)):
+            for c in range(1, len(matrix[0])):
+                if matrix[r][c] != matrix[r-1][c-1]:
                     return False
+        
+        return True        
+        
+
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        pre_row = matrix[0][:-1]
+        for r in range(1, len(matrix)):
+            cur_row = matrix[r][1:]
+            if pre_row != cur_row:
+                return False
+            pre_row = matrix[r][:-1]
         
         return True
